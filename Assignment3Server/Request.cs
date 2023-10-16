9c8ddfadad62ba647f63a573ed6d0d547b6626e9
xframee <alexander@kmax.dk>
2023-10-16 10:02:01 +0200

    changed date datatype to string

--- Assignment3Server/Request.cs
+++ Assignment3Server/Request.cs
@@ -4,6 +4,6 @@ public class Request
 {
     public string? Method { get; set; }
     public string? Path { get; set; }
-    public int Date { get; set; }
+    public string? Date { get; set; }
     public string? Body { get; set; }
 }
